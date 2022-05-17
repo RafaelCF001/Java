cf89ba76eab383e6a36f1518523005a49ece4e0d
Rafael Carvalho Ferreira <86693638+RafaelCF001@users.noreply.github.com>
2022-05-17 17:19:52 -0300

    Add files via upload

--- Experimento/src/laboratorio/mistura/Experimento.java
+++ Experimento/src/laboratorio/mistura/Experimento.java
@@ -1,5 +1,21 @@
 package laboratorio.mistura;
+import java.util.ArrayList;
 
 public class Experimento {
+	private ArrayList<Mistura> misturas = new ArrayList<Mistura>();
 
+	
+	public void setMistura(Mistura m) {
+		this.misturas.add(m);
+	}
+
+	public boolean registrarMistura(Mistura m) {
+		setMistura(m);
+		return this.misturas.contains(m);
+	}
+	public void exibirExperimento() {
+		for(int i = 0 ; i < misturas.size(); i++ ) {
+			System.out.println("Misturas: " + misturas.get(i));
+		}
+	}
 }
